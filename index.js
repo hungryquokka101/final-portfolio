aa6063049f3567ac79daa304abb3ab1c0855b6d6
Sarah <yishan_liu+cs1300@brown.edu>
2022-12-16 17:20:48 -0500

    feat: pushing to github

--- index.js
+++ index.js
@@ -11,3 +11,64 @@ document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventList
 	const lightbox = new Lightbox(el, options);
 	lightbox.show();
 }));
+
+// MAGIC TEXT JS
+let index = 0,
+    interval = 1000;
+
+const rand = (min, max) => 
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const animate = star => {
+  star.style.setProperty("--star-left", `${rand(-10, 100)}%`);
+  star.style.setProperty("--star-top", `${rand(-40, 80)}%`);
+
+  star.style.animation = "none";
+  star.offsetHeight;
+  star.style.animation = "";
+}
+
+for(const star of document.getElementsByClassName("magic-star")) {
+  setTimeout(() => {
+    animate(star);
+    
+    setInterval(() => animate(star), 1000);
+  }, index++ * (interval / 3))
+}
+
+/* -- ↓↓↓ If you want the sparkle effect to only occur on hover, replace lines 16 and on with this code ↓↓↓ -- */
+
+// let timeouts = [],
+//     intervals = [];
+
+// const magic = document.querySelector(".magic");
+
+// magic.onmouseenter = () => {
+//   let index = 1;
+  
+//   for(const star of document.getElementsByClassName("magic-star")) {
+//     timeouts.push(setTimeout(() => {  
+//       animate(star);
+      
+//       intervals.push(setInterval(() => animate(star), 1000));
+//     }, index++ * 300));
+//   };
+// }
+
+// magic.onmouseleave = onMouseLeave = () => {
+//   for(const t of timeouts) clearTimeout(t);  
+//   for(const i of intervals) clearInterval(i);
+  
+//   timeouts = [];
+//   intervals = [];
+// }
+
+// Rilkkuma JS
+(function($){
+	var section = $(".section"),
+			h1 = $("h1"),
+			img = $("img");
+	
+	TweenMax.from(img, 1, {width:100, autoAlpha: 0});
+	TweenMax.from(h1, 1, {y:100, autoAlpha:0, delay: 1, ease: Back.easeOut.config(2)});
+})(jQuery);
